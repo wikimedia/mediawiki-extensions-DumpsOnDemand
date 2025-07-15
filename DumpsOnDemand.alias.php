bb6ce5142c70f5dfbb1509354a289a4266b871b2
anterdc99 <xuminruixuan@live.cn>
2025-07-15 13:06:37 +0800

    DumpsOnDemand: Translate special page aliases into Chinese
    Change-Id: I4f0bc2700eeaf4db96f69cd74ebd240572f1d188

--- DumpsOnDemand.alias.php
+++ DumpsOnDemand.alias.php
@@ -14,3 +14,18 @@ $specialPageAliases['en'] = [
 $specialPageAliases['nl'] = [
 	'RequestDump' => [ 'DumpAanvragen' ]
 ];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'RequestDump' => [ 'RequestDump' ]
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'RequestDump' => [ '请求转储', '申请转储' ]
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'RequestDump' => [ '請求傾印', '申請傾印' ]
+];
